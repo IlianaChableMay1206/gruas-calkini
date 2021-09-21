7c626f13b5d8c73ba214ea7f29bf849687aa11f4
BrayanEscamilla <bescamilla481@gmail.com>
2021-09-21 14:00:48 -0500

    confirmar

--- Gruas-Calkini/src/app/app.module.ts
+++ Gruas-Calkini/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule
