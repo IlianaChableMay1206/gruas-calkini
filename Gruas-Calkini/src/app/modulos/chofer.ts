f62bc759c6776d95c9cfb68def56f8b682c61a87
Irma Zi Canul <80069339+IrmaZi@users.noreply.github.com>
2021-09-21 01:55:17 -0500

    Irma

--- Gruas-Calkini/src/app/modulos/chofer.ts
+++ Gruas-Calkini/src/app/modulos/chofer.ts
@@ -2,6 +2,16 @@ class chofer{
     constructor(
         public id: number,
         public nombre: string,
-        public apellido: string
+        public apellido: string,
+        public edad: number,
+        public rol: string,
+        public doc_licfederal: string,
+        public fecha_licfederal: string,
+        public num_licfederal: string,
+        public doc_licestatal: string,
+        public fecha_licestatal: string,
+        public num_licestatal: string,
+        public doc_ine: string,
+        public doc_curp: string
     ){}
 }
