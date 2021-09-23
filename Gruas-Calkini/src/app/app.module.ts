543184117656f303ef83cc86e9aae39ac8cd8bb3
Irma Zi Canul <80069339+IrmaZi@users.noreply.github.com>
2021-09-22 19:54:24 -0500

    irma

--- Gruas-Calkini/src/app/app.module.ts
+++ Gruas-Calkini/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-
+    RegistroComponent
   ],
   imports: [
     BrowserModule
