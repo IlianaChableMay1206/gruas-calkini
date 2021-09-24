0b2fb66bd4a37b364d6bb44016aebc1c48547f13
BrayanEscamilla <bescamilla481@gmail.com>
2021-09-24 01:09:33 -0500

    FRONTEND

--- Gruas-Calkini/src/app/app.module.ts
+++ Gruas-Calkini/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { routing, appRoutingProviders } from './app.routing';
 import { BrowserModule } from '@angular/platform-browser';
+import {SharedModule} from "./shared/shared.module";
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -9,17 +10,20 @@ import { HomeComponent } from './components/home/home.component';
 import { ErrorComponent } from './components/error/error.component';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegistroComponent,
     HomeComponent,
-    ErrorComponent
+    ErrorComponent,
+    
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    SharedModule
   ],
   providers: [
     appRoutingProviders
