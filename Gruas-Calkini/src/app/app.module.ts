6ec565cc4d6898035156442b7f7b579519f8808c
Irma Zi Canul <80069339+IrmaZi@users.noreply.github.com>
2021-09-23 19:26:59 -0500

    irma

--- Gruas-Calkini/src/app/app.module.ts
+++ Gruas-Calkini/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegistroComponent
+    RegistroComponent,
+    HomeComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
--- Gruas-Calkini/src/app/app.routing.ts
+++ Gruas-Calkini/src/app/app.routing.ts
@@ -5,13 +5,16 @@ import { Routes, RouterModule } from "@angular/router";
 //IMPORTAR COMPONENTES
 import { LoginComponent } from "./components/login/login.component";
 import { RegistroComponent } from "./components/registro/registro.component";
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 
 //DEFINIR RUTAS
 const appRoutes: Routes =[
     {path: '', component: LoginComponent},
     {path: 'inicio', component: LoginComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'registro', component: RegistroComponent}
+    {path: 'registro', component: RegistroComponent},
+    {path: '**', component: ErrorComponent}
 ];
 
 //EXPORTAR CONFIGURACIÓN
